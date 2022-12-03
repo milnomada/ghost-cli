842b90600f7e1d4010924fd2972895574ef4a75a
milnomada <milnomada@users.noreply.github.com>
2022-12-03 14:23:03 +0000

    Bump setup.py to 0.1.66
    Files changed:
    A	0.1.66
    M	setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ghost-cli',
-    version='0.1.65',
+    version='0.1.66',
     description='A Python Client for the Ghost Admin API',
     url='https://github.com/milnomada/ghost-cli',
     author='Milnomada.org',
