fcc65406c2a690342a002c64b369244eab8b0da5
Milnomada <info@milnomada.io>
2022-12-03 21:57:39 +0100

    Sync tags along project

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ghost-cli',
-    version='0.2.0',
+    version='0.3.1',
     description='A Python Client for the Ghost Admin API',
     url='https://github.com/milnomada/ghost-cli',
     author='Milnomada.org',
